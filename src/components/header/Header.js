80985cc2945fb83b0e436d618fb07c0afb64c028
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-07-04 20:21:28 +0300

    edit Header

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -2,9 +2,7 @@ import userIcon from '../../assets/images/icons/user1.png';
 import mainlogo from '../../assets/main-logo.svg';
 
 import styles from './header.module.css';
-import Button from '../button/Button.js';
-import themeBtn from '../button/themeBtn.module.css';
-import Main from '../main/Main.js';
+
 
 
 import { Link } from "react-router-dom";
@@ -44,18 +42,3 @@ export default function Header() {
 }
 
 
-
-{/* <div >
-                        <Button
-                            theme={themeBtn.btn_wordslist}
-                            title="Список" />
-                    </div>
-
-                    <div >
-                        <Button
-                            theme={themeBtn.btn_learning}
-                            title="Учить" />
-                    </div>
-                    <div className="user_icon">
-                        <img src={userIcon} alt="иконка пользователя"></img>
-                    </div> */}
