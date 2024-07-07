03596545d15082941eff27c1aa591ac8ab53cf97
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-07-07 14:08:37 +0300

    mare changes in Learning

--- src/components/cardTranslation/CardTranslation.js
+++ src/components/cardTranslation/CardTranslation.js
@@ -11,10 +11,7 @@ export default function CardTranslation(props) {
             <div className={style.wrapper}>
                 <div className={style.card_cover}>
                     <span className={style.card_russian}>{props.russian}</span>
-                    {/* <fieldset className={style.fieldset}>
-                        <label for='chbx'>Вы знали перевод?</label>
-                        <input type='checkbox' id='chbx' className={style.chbx} onChange={(e) => handleCheckedState(e)} />
-                    </fieldset> */}
+
                     <input ref={input => input && input.focus()} type='button' value='Я знаю это слово' className={style.btn} onClick={props.handleWordIsLearned} />
 
                 </div>
