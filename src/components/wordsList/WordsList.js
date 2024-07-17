7b59e4e820a261f3da8654e9f06c5e263b2c1983
Marie Faleeva <134315162+Laydena@users.noreply.github.com>
2024-07-17 14:40:33 +0300

    add API

--- src/components/wordsList/WordsList.js
+++ src/components/wordsList/WordsList.js
@@ -6,9 +6,7 @@ import style from './wordslist.module.css';
 
 import editIcon from '../../assets/images/icons/edit-icon.png';
 import deleteIcon from '../../assets/images/icons/delete-icon.png';
-import saveIcon from '../../assets/images/icons/save-icon.png';
-import cancelIcon from '../../assets/images/icons/cancel-icon.png';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function Wordslist() {
     const [editStates, setEditStates] = useState({});
@@ -20,35 +18,58 @@ export default function Wordslist() {
         }));
     };
 
+    const [words, setWords] = useState([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    useEffect(() => {
+        fetch('http://itgirlschool.justmakeit.ru/api/words')
+            .then(response => {
+                if (response.ok) { //Проверяем, что код ответа 200
+                    return response.json();
+                } else {
+                    throw new Error('Something went wrong ...');
+                }
+            })
+            .then((response) => {
+                setWords(response)
+                setLoading(false)
+            })
+            .catch(error => setError(error));
+    })
+
     return (
         <>
-            <div className="cards-container" id="cards-container">
-                {data.map((item, id) =>
-                    editStates[item.id]
-                        ? (
-                            <div className={style.line_editable} key={id}>
-                                <WordlineEditable
-                                    english={item.english}
-                                    transcription={item.transcription}
-                                    russian={item.russian} />
+            {loading ?
+                (<p>Loading ...</p>)
+                :
+                (<div className="cards-container" id="cards-container">
+                    {words.map((word) =>
+                        editStates[word.id]
+                            ? (
+                                <div className={style.line_editable} key={word.id}>
+                                    <WordlineEditable
+                                        english={word.english}
+                                        transcription={word.transcription}
+                                        russian={word.russian} />
 
-                            </div>
-                        )
-                        : (
-                            <div className={style.line} key={id}>
-                                <Wordline {...item} />
-                                <div className={style.icons}>
-                                    <button className={style.btn} onClick={() => handleLineEditClicked(item.id)}>
-                                        <img src={editIcon} alt="иконка редактировать"></img>
-                                    </button>
-                                    <button className={style.btn}>
-                                        <img src={deleteIcon} alt="иконка удалить"></img>
-                                    </button>
                                 </div>
-                            </div>
-                        ),
-                )}
-            </div>
+                            )
+                            : (
+                                <div className={style.line} key={word.id}>
+                                    <Wordline {...word} />
+                                    <div className={style.icons}>
+                                        <button className={style.btn} onClick={() => handleLineEditClicked(word.id)}>
+                                            <img src={editIcon} alt="иконка редактировать"></img>
+                                        </button>
+                                        <button className={style.btn}>
+                                            <img src={deleteIcon} alt="иконка удалить"></img>
+                                        </button>
+                                    </div>
+                                </div>
+                            ),
+                    )}
+                </div>)}
 
             <div className={style.center}>
                 <AddWordBtn />
